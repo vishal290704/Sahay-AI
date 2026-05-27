461ccf1ac9990a090dc2928307227abf3f2233ca
Vishal290704 <vsjee2002@gmail.com>
2026-05-27 15:36:17 +0530

    changes minor

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -10,6 +10,7 @@ export async function GET(req:NextRequest) {
         return NextResponse.json({message:"Code is not found"},{status:400})
     }
     const session = await scalekit.authenticateWithCode(code, redirectUri)
+    console.log(session)
     const response = NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}`)
     response.cookies.set("access_token", session.accessToken, {
         httpOnly:true,
