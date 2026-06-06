a4d491b7134972dbbf0114d28fce0f092d14abc6
Vishal290704 <vsjee2002@gmail.com>
2026-06-06 15:17:31 +0530

    Add embed demo page and update chatbot widget

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,13 +1,33 @@
 (function(){
     const api_Url = "http://localhost:3000/api/chat"
     const scriptTag = document.currentScript;
-    const ownderId = scriptTag.getAttribute("data-owner-id")
+    const ownerId = scriptTag.getAttribute("data-owner-id")
 
-    if(!ownderId){
+    if(!ownerId){
         console.log("Owner Id is not found")
         return
     }
 
     const button = document.createElement("div")
     button.innerHTML = "💬"
+    document.body.append(button)
+
+    Object.assign(button.style, {
+        position: "fixed",
+        bottom: "24px",
+        right: "24px",
+        width: "56px",
+        height: "56px",
+        borderRadius: "50%",
+        background: "#000",
+        color: "#fff",
+        dispay: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: "pointer",
+        fontSize: "22px",
+        boxShadow: "0 15px 40px rgba(0,0,0,0.35)",
+        zIndex: "999999"
+
+    })
 })()
