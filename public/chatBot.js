cac9f7811ff85c5027f8143a860016f6c5409ea2
Vishal290704 <vsjee2002@gmail.com>
2026-06-07 21:09:33 +0530

    Fix

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,6 +1,6 @@
 
 (function () {
-  const api_Url = "http://localhost:3000/api/chat";
+  const api_Url = "https://sahay-ai-alpha.vercel.app/api/chat";
   const scriptTag = document.currentScript;
   const ownerId = scriptTag.getAttribute("data-owner-id");
 
